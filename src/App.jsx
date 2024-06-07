164a7b308324d01979d2f7de17fbca896a7b8eef
shubhamdhobale2021 <shubhamdhobale2021.it@mmcoe.edu.in>
2024-06-07 21:12:42 +0530

    Add Responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import './App.css';
 import { Navbar, About, Projects, Contact, Skills, Footer } from './components/index.js';
 import Home from './components/Home.jsx';
 import { motion } from 'framer-motion';
-// import { Outlet, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <motion.div className="flex flex-col items-center justify-center w-full tracking-wider min-h-screen flex-grow" >
+    <motion.div className="flex flex-col items-center justify-center w-full min-h-screen bg-gray-100" >
       <Navbar />
-      {/* <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes> */}
-      <Home />
-      <About initial={{opacity:0}} animate={{opacity: 1}} transition={{duration: 3}}/>
-      <Skills initial={{opacity:0}} animate={{opacity: 1}} transition={{duration: 3}}/> 
-      <Projects initial={{opacity:0}} animate={{opacity: 1}} transition={{duration: 3}}/>
-      <Contact initial={{opacity:0}} animate={{opacity: 1}} transition={{duration: 3}}/>
-      <Footer initial={{opacity:0}} animate={{opacity: 1}} transition={{duration: 3}}/>
-   
+      <div className="w-full">
+        <Home />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
     </motion.div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="text-black w-full p-4 md:p-6 fixed top-0 left-0 border border-black shadow-lg z-50 bg-white">
       <div className="container mx-auto flex flex-row md:flex-row justify-between items-center">
-        <img src={logo} alt="logo" className='w-32 md:w-44 mb-4 md:mb-0 md:ml-10 ml-16'/>
+        <img src={logo} alt="logo" className='w-32 md:w-44 mb-4 md:mb-0 md:ml-10  '/>
         <ul className="flex flex-row gap-4 md:flex-row md:space-x-4 text-lg md:text-xl opacity-75 mb-4 md:mb-0 mr-4 md:mr-20">
           <li className='hover:underline hover:text-gray-900 transition-all duration-1000'>
             <a href="/home">Home</a>
