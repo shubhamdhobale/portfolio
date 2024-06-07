cd53438b771e95b1c6af72b92be7fab6a981795e
shubhamdhobale2021 <shubhamdhobale2021.it@mmcoe.edu.in>
2024-06-07 21:33:02 +0530

    Add Responsiveness

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,11 +9,11 @@ const Contact = () => {
     <motion.div id='contact' className="pt-20 px-4 md:px-8 lg:px-16 flex flex-col justify-center items-center w-full" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 10 }}>
       <p className="p-1 opacity-90">Get in Touch</p>
       <h1 className="text-3xl md:text-5xl font-bold">Contact Me</h1>
-      <div className='flex flex-row mt-20 md:flex-row justify-center items-center gap-8'>
-        <div className='w-full md:w-1/2'>
+      <div className='flex flex-row mt-20 md:flex-row justify-center items-center gap-8 w-full'>
+        <div className='w-96 md:w-1/2'>
           <img src={img} alt="" className='mb-8 w-full' />
         </div>
-        <div className='w-full md:w-1/2'>
+        <div className='w-2/4 md:w-1/2'>
           <form action="" className='flex flex-col gap-6'>
             <input type="text" className='border border-black p-4 rounded-xl' placeholder='Name' />
             <textarea name='message' rows={3} className='border border-black p-4 rounded-xl' placeholder='Message'></textarea>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="mt-10 w-full">
-      <p className="text-center text-white p-2 mt-2 bg-gray-500 w-auto md:w-auto">Copyright &copy; 2024 Shubham Dhobale. All rights reserved.</p>
+      <p className=" text-black p-2 mt-2 ml-4  w-full md:w-auto">Copyright &copy; 2024 Shubham Dhobale. All rights reserved.</p>
     </div>
   );
 };
