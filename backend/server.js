53903984af70a52aa1269192cef23e6e0a5264c4
shubhamdhobale2021 <shubhamdhobale2021.it@mmcoe.edu.in>
2024-07-02 22:16:07 +0530

    add backend and fix minor bugs

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 // MongoDB Connection
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGO_URL}`);
+    await mongoose.connect(`${import.meta.env.MONGO_URL}`);
     console.log(`DB Connected ${mongoose.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error", error);
